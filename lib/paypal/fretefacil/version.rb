33765f01d83d822fea9acd2a38278e5829e1aedf
Prodis <prodis@gmail.com>
2011-08-22 01:24:19 -0300

    Version.

---
+++ lib/paypal/fretefacil/version.rb
@@ -0,0 +1,11 @@
+# encoding: UTF-8
+module PayPal
+  module FreteFacil
+    module Version
+      MAJOR = 0
+      MINOR = 0
+      PATCH = 1
+      VERSION = [MAJOR, MINOR, PATCH].join(".")
+    end
+  end
+end
